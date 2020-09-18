ca68bec8a95f6aaaea793c1ec408684f0a3602a7
rupalinaphade <47784374+rupalinaphade@users.noreply.github.com>
2020-09-18 17:32:21 +0200

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="TabContainer">
+    <tabs>
+      
+      <tab title=" About Us "><AboutUs1></AboutUs1></tab>
+      <tab title="Vision & Mission"><Vision></Vision></tab>
+      <tab title="Our Products"><Product></Product></tab>
+      <tab title="Customer"><Customer></Customer></tab>
+      <tab title="Contact Us"><Contact></Contact></tab>
+    </tabs>
+  </div>
+</template>
+
+<script>
+import Tab from './Tab.vue'
+import Tabs from './Tabs.vue'
+import AboutUs1 from './Aboutus1.vue'
+import Vision from './Vision'
+import Contact from './ContactUs'
+import Product from './Products'
+import Customer from './Customer'
+export default {
+  components: {
+    Tab,
+    Tabs,
+    AboutUs1,
+    Vision,
+    Contact,
+    Product,
+    Customer
+    
+  }
+}
+</script>
+<style scoped>
+.TabContainer{
+  margin-top: 10px;
+}
+.textcontent{
+    text-align: justify;
+}
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,43 @@
+<template lang="html" >
+   
+        <div class="row">
+             <br/>
+            <br/>
+            <div class="block">
+            <img alt="Machine1" src="@/machineImages/OasisLogo.jpg">
+            <p>Machine related information </p>
+            </div>
+            <div class="block">
+            <img alt="Machine2" src="@/machineImages/OasisLogo.jpg">
+            <p>Machine related information </p>
+            </div>
+
+            <div class="block">
+            <img alt="Machine3" src="@/machineImages/OasisLogo.jpg">
+            <p>Machine related information </p>
+            </div>
+            <div class="block">
+            <img alt="Machine4" src="@/machineImages/OasisLogo.jpg">
+            <p>Machine related information </p>
+            </div>
+           
+        </div>
+   
+</template>
+<style >
+.row {
+  width: 100%;
+ margin-bottom: 40%;
+}
+.block {
+  width: 35%;
+  float: left;
+}
+.tabcontent {
+    padding-left: 5%;
+    padding-right: 5%;
+    border: 0px solid #ccc !important;
+    border-radius: 0px !important;
+  box-shadow: 0px 0px 0px #e1e1e1 !important;
+}
+</style>
